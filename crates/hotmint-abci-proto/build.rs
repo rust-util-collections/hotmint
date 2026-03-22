3e9af1a83773503e6278607d43c3958834692843
ktmlm <hui.fan@mail.ru>
2026-03-22 13:17:55 +0800

    fix: include abci.proto in crate for cargo publish
    The build.rs referenced ../../proto/abci.proto which doesn't exist
    in the packaged tarball, breaking `cargo publish` verification.

--- crates/hotmint-abci-proto/build.rs
+++ crates/hotmint-abci-proto/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     let manifest_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let proto_dir = manifest_dir.join("../../proto");
+    let proto_dir = manifest_dir.join("proto");
     let proto_file = proto_dir.join("abci.proto");
     prost_build::compile_protos(&[proto_file], &[proto_dir]).unwrap();
 }
